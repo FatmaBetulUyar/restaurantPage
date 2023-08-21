7e6b398121315e2ecf341682a200c8b5327b5ad4
Betül <69481739+FatmaBetulUyar@users.noreply.github.com>
2023-08-21 23:51:05 +0300

    html elements created using js.

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,50 @@
-console.log("hello");
+document.addEventListener("DOMContentLoaded", function () {
+  const content = document.getElementById("content");
+
+  //subcontent
+  const subContent = document.createElement("div");
+  subContent.classList.add("subContent");
+
+  //subcontent > imageDiv
+  const imageDiv = document.createElement("div");
+  imageDiv.classList.add("restaurantImage");
+
+  //subcontent > imageDiv > img
+  const image = document.createElement("img");
+  image.classList.add("headerImg");
+  image.src =
+    "https://cdn.pixabay.com/photo/2018/03/24/16/08/cake-3257019_1280.jpg";
+  image.alt = "headerImg";
+  image.srcset = "";
+
+  imageDiv.appendChild(image);
+
+  //subcontent > header
+  const headerDiv = document.createElement("div");
+  headerDiv.classList.add("header");
+
+  //subcontent > header > h1
+  const header = document.createElement("h1");
+  header.textContent = "betul's bakery";
+
+  headerDiv.appendChild(header);
+
+  //subcontent > about
+  const aboutDiv = document.createElement("div");
+  aboutDiv.classList.add("aboutText");
+
+  //subcontent > about > p
+  const aboutText = document.createElement("p");
+  aboutText.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dignissimos consequuntur odio in et corrupti vero ipsam perspiciatis tempora temporibus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt possimus soluta hic consequuntur exercitationem ipsum, natus dipiscioluptatibus magni quaerat provident eaque itaque cupiditate doloribus iure ratione, unde esse?";
+
+  aboutDiv.appendChild(aboutText);
+
+  // add all to subcontent
+  subContent.appendChild(imageDiv);
+  subContent.appendChild(header);
+  subContent.appendChild(aboutDiv);
+
+  // add subcontent to content
+  content.appendChild(subContent);
+});
